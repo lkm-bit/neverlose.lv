94c1e94e76424412dd3821de1d04b0fb082d324c
lkm-bit <136848849+lkm-bit@users.noreply.github.com>
2023-09-03 13:31:43 +0300

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,3 +1,3 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/lkm-bit/neverlose.lv/main/neverlosemain.lua?token=GHSAT0AAAAAACGZYHFYMLBCI2FCZ6SI3C7KZHTCVYQ"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/lkm-bit/neverlose.lv/main/neverlosemain.lua"))()
 
 --this should work
